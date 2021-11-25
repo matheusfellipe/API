ca1dd5d02144a0926e7eacb0318946688ae0bc66
Matheus Fellipe <matheus060399@gmail.com>
2021-11-25 17:44:35 -0400

    Login_Route

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,19 @@ const express = require('express');
 
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
-routes.get('/ongs', OngController.index);
+routes.post('/sessions', SessionController.create);
 
+routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.index);
+routes.get('/profile', ProfileController.index);
 
+routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
 routes.delete('/incidents/:id', IncidentController.delete);
 
